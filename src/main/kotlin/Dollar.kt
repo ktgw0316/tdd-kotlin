6874d19cdc9e5112ab29bc719a445314d1968478
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2022-11-04 12:33:38 +0900

    feat: Implement Dollar.equals
    * Green $5 = $5
    
    Fix #6

--- src/main/kotlin/Dollar.kt
+++ src/main/kotlin/Dollar.kt
@@ -1,5 +1,6 @@
 package money
 
 class Dollar(val amount: Int) {
+    override fun equals(other: Any?) = amount == (other as? Dollar)?.amount
     operator fun times(multiplier: Int) = Dollar(amount * multiplier)
 }
