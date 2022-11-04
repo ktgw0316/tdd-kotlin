d10ace1931e6ded33949c51ecbffc2bdba5096bc
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2022-11-04 12:44:16 +0900

    refactor(test): Correct assertion usage

--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -2,24 +2,25 @@ package money
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 
 class MoneyTest : FreeSpec({
 
     "Multiplication" - {
         val five = Dollar(5)
         "$5 * 2 = $10" {
-            (five * 2 == Dollar(10)) shouldBe true
+            five * 2 shouldBe Dollar(10)
         }
         "$5 * 3 = $15" {
-            (five * 3 == Dollar(15)) shouldBe true
+            five * 3 shouldBe Dollar(15)
         }
     }
     "Equality" - {
         "$5 = $5" {
-            (Dollar(5) == Dollar(5)) shouldBe true
+            Dollar(5) shouldBe Dollar(5)
         }
         "$5 != $6" {
-            (Dollar(5) == Dollar(6)) shouldBe false
+            Dollar(5) shouldNotBe Dollar(6)
         }
     }
 })
