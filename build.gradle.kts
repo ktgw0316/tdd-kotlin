e51ce0d49b93dd978ce7a945a39219213b4ed1f0
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2022-11-04 10:38:24 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+val kotestVersion = "5.5.3"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
