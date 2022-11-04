dca1bafa8c06565ff01e5a3bceb13e9a62ff0f9d
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2022-11-04 14:39:59 +0900

    feat: Introduce currency
    Fix #15

--- src/main/kotlin/Dollar.kt
+++ src/main/kotlin/Dollar.kt
@@ -1,5 +1,10 @@
 package money
 
+import java.util.*
+
 class Dollar(private val amount: Int): Money(amount) {
+    override val currency: String
+        get() = "USD"
+
     override operator fun times(multiplier: Int) = Dollar(amount * multiplier)
 }
--- src/main/kotlin/Franc.kt
+++ src/main/kotlin/Franc.kt
@@ -1,5 +1,8 @@
 package money
 
 class Franc(private val amount: Int) : Money(amount) {
+    override val currency: String
+        get() = "CHF"
+
     override operator fun times(multiplier: Int) = Franc(amount * multiplier)
 }
