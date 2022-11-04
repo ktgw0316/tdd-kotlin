9b9bad7f6d1507b423a6a06dbec13ce432857c2f
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2022-11-04 12:06:30 +0900

    feat: Green $5 * 2 = $10
    Fix #2

--- src/main/kotlin/Dollar.kt
+++ src/main/kotlin/Dollar.kt
@@ -1,5 +1,8 @@
 package money
 
 class Dollar(var amount: Int) {
-    operator fun times(multiplier: Int): Dollar = TODO()
+    operator fun times(multiplier: Int): Dollar {
+        amount *= multiplier
+        return this
+    }
 }
