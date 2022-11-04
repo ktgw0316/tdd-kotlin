29829036130139d7430989134fd15f067a0fc3c0
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2022-11-04 14:48:56 +0900

    refactor(red): Common times
    * Take currency as a constructor param
    * Remove Dollar and Franc classes

--- src/main/kotlin/Franc.kt
+++
@@ -1,8 +0,0 @@
-package money
-
-class Franc(private val amount: Int) : Money(amount) {
-    override val currency: String
-        get() = "CHF"
-
-    override operator fun times(multiplier: Int) = Franc(amount * multiplier)
-}
