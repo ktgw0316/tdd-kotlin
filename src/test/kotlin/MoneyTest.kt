8ad5feb39a158bfadd017180bd58ffa4488d3220
Masahiro Kitagawa <masahiro_kitagawa@nidek.co.jp>
2022-11-04 12:10:11 +0900

    test(red): $5 * 3 = $15

--- src/test/kotlin/MoneyTest.kt
+++ src/test/kotlin/MoneyTest.kt
@@ -6,10 +6,14 @@ import io.kotest.matchers.shouldBe
 class MoneyTest : FreeSpec({
 
     "Multiplication" - {
+        val five = Dollar(5)
         "$5 * 2 = $10" {
-            val five = Dollar(5)
-            five * 2
-            five.amount shouldBe 10
+            val product = five * 2
+            product.amount shouldBe 10
+        }
+        "$5 * 3 = $15" {
+            val product = five * 3
+            product.amount shouldBe 15
         }
     }
 })
